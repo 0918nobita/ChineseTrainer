58a268c3977760a90d3f7d1015e544f31987e3ed
Kodai Matsumoto <nobita.0918@gmail.com>
2024-07-17 00:37:47 +0900

    2Dゲーム開発のチュートリアルを完了した

--- mob.gd
+++ mob.gd
@@ -1,14 +1,20 @@
 extends RigidBody2D
 
-var animated_sprite_2d: AnimatedSprite2D
+class_name Mob
+
+const Scene := preload('res://mob.tscn')
+
+static func instantiate() -> Mob:
+    return Scene.instantiate() as Mob
+
+@onready var sprite := $AnimatedSprite2D as AnimatedSprite2D
 
 func _ready() -> void:
-    animated_sprite_2d = $AnimatedSprite2D as AnimatedSprite2D
-    var mob_types := animated_sprite_2d.sprite_frames.get_animation_names()
+    var mob_types := sprite.sprite_frames.get_animation_names()
     var animation_index := randi() % mob_types.size()
-    animated_sprite_2d.play(mob_types[animation_index])
+    sprite.play(mob_types[animation_index])
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
     pass
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
--- player.gd
+++ player.gd
@@ -1,17 +1,17 @@
 extends Area2D
 
+class_name Player
+
 signal hit
 
 @export var speed := 400
-var screen_size: Vector2
-var animated_sprite_2d: AnimatedSprite2D
-var collision_shape_2d: CollisionShape2D
+
+@onready var screen_size := get_viewport_rect().size
+@onready var sprite := $AnimatedSprite2D as AnimatedSprite2D
+@onready var collision := $CollisionShape2D as CollisionShape2D
 
 func _ready() -> void:
     hide()
-    screen_size = get_viewport_rect().size
-    animated_sprite_2d = $AnimatedSprite2D as AnimatedSprite2D
-    collision_shape_2d = $CollisionShape2D as CollisionShape2D
 
 func _process(delta: float) -> void:
     var velocity := Vector2.ZERO
@@ -28,33 +28,33 @@ func _process(delta: float) -> void:
     if velocity.length() > 0:
         # 斜め移動でも同じスピードになるように予め normalize する
         velocity = velocity.normalized() * speed
-        animated_sprite_2d.play()
+        sprite.play()
     else:
-        animated_sprite_2d.pause()
+        sprite.pause()
 
     position += velocity * delta
     position = position.clamp(Vector2.ZERO, screen_size)
 
     if velocity.x != 0 and velocity.y != 0:
-        animated_sprite_2d.animation = "up"
-        animated_sprite_2d.flip_h = velocity.y > 0
-        animated_sprite_2d.flip_v = velocity.x < 0
+        sprite.animation = 'up'
+        sprite.flip_h = velocity.y > 0
+        sprite.flip_v = velocity.x < 0
     if velocity.x != 0:
-        animated_sprite_2d.animation = "walk"
-        animated_sprite_2d.flip_v = false
-        animated_sprite_2d.flip_h = velocity.x < 0
+        sprite.animation = 'walk'
+        sprite.flip_v = false
+        sprite.flip_h = velocity.x < 0
     if velocity.y != 0:
-        animated_sprite_2d.animation = "up"
-        animated_sprite_2d.flip_v = velocity.y > 0
+        sprite.animation = 'up'
+        sprite.flip_v = velocity.y > 0
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
     hide()
     hit.emit()
     # 衝突判定の途中でコリジョンを無効化するとエラーが発生するため、
     # フレーム終了時までコリジョン無効化処理を遅延する
-    collision_shape_2d.set_deferred("disabled", false)
+    collision.set_deferred('disabled', false)
 
 func start(pos: Vector2) -> void:
     position = pos
     show()
-    collision_shape_2d.disabled = false
+    collision.disabled = false
